5251a9bd447d029a997d5730a88329ed6058bfc3
Darko Todoroski <daredarepp@yahoo.com>
2017-10-19 21:53:22 +0200

    Add scroll to the small catalog window and fix the headings

--- public/javascripts/jquery/my_index_jquery.js
+++ public/javascripts/jquery/my_index_jquery.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+
+    // Scroll images right
+
+    $('#arrow_right').click(function(){
+        event.preventDefault();
+        let scroll = $('#preview_images').scrollLeft();
+        scroll += 285;
+        $('#preview_images').animate({scrollLeft: scroll},200);
+    });
     
-})
+    // Scroll images left
+
+    $('#arrow_left').click(function(){
+        event.preventDefault();
+        let scroll = $('#preview_images').scrollLeft();
+        scroll -= 285;
+        $('#preview_images').animate({scrollLeft: scroll},200);
+    });
+
+});
