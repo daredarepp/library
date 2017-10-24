6bc9e893f1719c5cd5257a138f7f62a31aac77ac
Darko Todoroski <daredarepp@yahoo.com>
2017-10-24 23:39:08 +0200

    Add basic get routes and controllers for the catalog books, authors and genres

--- routes/catalog.js
+++ routes/catalog.js
@@ -7,5 +7,19 @@ var catalog_controllers = require('./controllers/catalog_controllers');
 
 router.get('/', catalog_controllers.catalog_get);
 
+/*  GET Books  */
+
+router.get('/books', catalog_controllers.catalog_books_get);
+
+/* GET Authors */
+
+router.get('/authors', catalog_controllers.catalog_authors_get);
+
+/* GET Genres */
+
+router.get('/genres', catalog_controllers.catalog_genres_get);
+
+
+
 
 module.exports = router;
--- routes/controllers/catalog_controllers.js
+++ routes/controllers/catalog_controllers.js
@@ -1,6 +1,55 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var Book = require('/Users/Darko/Desktop/Library/library/models/book');
+var Author = require('/Users/Darko/Desktop/Library/library/models/author');
+var Genre = require('/Users/Darko/Desktop/Library/library/models/genre');
 
 /* GET catalog page. */
 
 module.exports.catalog_get = function(req,res,next){
     res.render('catalog',{title: "Catalog"});
+};
+
+/* GET Books */
+
+module.exports.catalog_books_get = function(req, res, next){
+
+    var uri = 'mongodb://localhost/mydb';
+    var options = { useMongoClient: true };
+    
+    mongoose.connect(uri, options);
+    Book.find(function(err, result){
+        if(err) throw err;
+        res.json(result);
+    });
+};
+
+/* GET Authors */
+
+module.exports.catalog_authors_get = function(req, res, next){
+
+    var uri = 'mongodb://localhost/mydb';
+    var options = { useMongoClient: true };
+    
+    mongoose.connect(uri, options);
+    Author.find(function(err, result){
+        if(err) throw err;
+        res.json(result);
+    });
+    
+};
+
+/* GET Genres */
+
+module.exports.catalog_genres_get = function(req, res, next){
+
+    var uri = 'mongodb://localhost/mydb';
+    var options = { useMongoClient: true };
+    
+    mongoose.connect(uri, options);
+    Genre.find(function(err, result){
+        if(err) throw err;
+        res.json(result);
+    });
+
 };
