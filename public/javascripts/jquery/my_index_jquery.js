5706ddc61070a262bc4237e89cd910ab6a55caf3
Darko Todoroski <daredarepp@yahoo.com>
2017-10-24 23:37:10 +0200

    Add show/hide button on the homepage windows

--- public/javascripts/jquery/my_index_jquery.js
+++ public/javascripts/jquery/my_index_jquery.js
@@ -6,18 +6,40 @@ $(document).ready(function(){
 
     $('#arrow_right').click(function(){
         event.preventDefault();
-        let scroll = $('#preview_images').scrollLeft();
+        let scroll = $('#preview_books').scrollLeft();
         scroll += 290;
-        $('#preview_images').animate({scrollLeft: scroll},200);
+        $('#preview_books').animate({scrollLeft: scroll},200);
     });
     
     // Scroll images left
 
     $('#arrow_left').click(function(){
         event.preventDefault();
-        let scroll = $('#preview_images').scrollLeft();
+        let scroll = $('#preview_books').scrollLeft();
         scroll -= 290;
-        $('#preview_images').animate({scrollLeft: scroll},200);
+        $('#preview_books').animate({scrollLeft: scroll},200);
     });
+
+    // Hide/Show window body
+    $('a.show_hide_button').click(function(){
+
+        event.preventDefault();
+
+        var window = $(this).parents('.window');
+        var button = $(this);
+
+        // Toggle the window body
+        window
+        .children('div.window_body')
+        .toggle();
+
+        // Change the arrow direction
+        if(button.text() === 'keyboard_arrow_up'){
+            button.text('keyboard_arrow_down');
+        }else{
+            button.text('keyboard_arrow_up');
+        };
+        
+    })
 
 });
