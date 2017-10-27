28c4fd8b1e941d6771fa826300d9905f4fd3d89d
Darko Todoroski <daredarepp@yahoo.com>
2017-10-28 01:35:06 +0200

    Improve catalog navigation

--- public/javascripts/jquery/my_catalog_jquery.js
+++ public/javascripts/jquery/my_catalog_jquery.js
@@ -1,77 +1,98 @@
 /* Catalog page */
 
 $(document).ready(function(){
-
-    // When clicking a selection link
-    $('a.selection').click(function(){
-            
-        open($(this).parent().attr('id'));
-    });
     
-    // Open selected window
-    function open(current) {
-        
+    // when selecting a category
+    $('.selection_window a, .selection_bar a').click(function(){
+
         event.preventDefault();
+        
+        // If the link is already active, do nothing
+        if($(this).attr('class').indexOf('active') > -1){
 
-        // Hide the selection link from the window
-        var link = $(`div#${current} a.selection`);
-        link.toggle();
-
-        // Hide the other windows
-        var otherWindows = $('.window').not(`#${current}`);
-        otherWindows.toggle();
-
-        // Customize the current window
-        var window = $(`div#${current}`);
-
-        var previousClass = window.attr('class');
-        var newClass = `col-md-8 col-md-offset-2 window`
-
-        var windowHeading = $('<h2></h2>')
-        .addClass('window_heading')
-        .html(link.html());
-
-        var windowCloseButton = $('<a></a>')
-        .attr({'href':'#', 'id':'close_button'})
-        .addClass('material-icons')
-        .text('close');
-
-        var windowBody = $('<div></div>')
-        .addClass('window_body')
-        .css('text-align','left');
-
-        window
-        .removeClass(previousClass)
-        .addClass(newClass)
-        .append(windowHeading)
-        .append(windowCloseButton)
-        .append(windowBody);
-
-        // Fill the window body with data 
-        $.get(link.attr('href'), function(data, status){
-            
-            if(status === 'error'){
-                windowBody.text('Sorry, something went wrong.');
-                return;
+            return;
+        
+        // Otherwise open a catalog for the selected link
+        }else{
+            openCatalog($(this).attr('class'));
             };
             
-            var finalData = JSON.stringify(data);
-            windowBody.text(finalData);
+            
         });
         
-        // When clicking the close button
-        windowCloseButton.click(function(){
+        function openCatalog(active){
+                        
+            // Grab reference to the existing elements
+            var wrapper = $('.catalog_wrapper');
+            var selectionWindows = $('.selection_window');
+            var selectionBar = ($('.selection_bar'));
+            var selectionBarLinks = selectionBar.children('a');
+            var activeLink = selectionBarLinks.filter(`.${active}`);
+            var path = activeLink.attr('href');
+
+            // Remove any previously created catalogs
+            $('.catalog').remove();
+
+            // Hide the selection windows and show the selection bar
+            selectionWindows.hide();
+            selectionBar.show()
+
+            // Style the active link
+            selectionBarLinks.removeClass('active');
+            activeLink.addClass('active');
+
+            // Create new catalog and elements for it
+            var catalog = $('<div></div>')
+            .addClass('window col-xs-12 col-md-8 col-md-offset-2 catalog');
+
+            var catalogTitle = $('<h2></h2>')
+            .addClass('window_title')
+            .html(activeLink.html());
+
+            var catalogCloseButton = $('<a></a>')
+            .attr({'id':'close_button', 'href':'#', 'title':'Close'})
+            .addClass('material-icons')
+            .text('close');
+
+            var catalogBody = $('<div></div>')
+            .addClass('window_body');
+
+            // Append the created elements to the catalog
+            catalog
+            .append(catalogTitle)
+            .append(catalogCloseButton)
+            .append(catalogBody);
+
+            // Fill the catalog body with ajax data 
+            $.get(path, function(data, status){
+
+                if(status === 'error'){
+                    catalogBody.text('Sorry, something went wrong.');
+                    return;
+                };
+                
+                var para = $('<p></p>').text(JSON.stringify(data));
+                catalogBody.html(para);
+            });
             
-            event.preventDefault();
+            // Append the catalog to the main wrapper
+            wrapper.append(catalog);
+
+            // history.pushState({wrapper : wrapper.html()}, '', `/catalog/${active}`); 
+
+            // When clicking the close button
+            catalogCloseButton.click(function(){
+                
+                event.preventDefault();
+                
+                // Remove the catalog 
+                catalog.remove();
+                activeLink.removeClass('active');
+
+                // Hide the selection bar and show the selection windows
+                selectionBar.hide();
+                selectionWindows.show();
             
-            // Undo everything
-            windowHeading.remove();
-            windowCloseButton.remove();
-            windowBody.remove();
-            window.removeClass(newClass);
-            window.addClass(previousClass);
-            link.toggle();
-            otherWindows.toggle();
         });
 
     };
--- public/javascripts/jquery/my_index_jquery.js
+++ public/javascripts/jquery/my_index_jquery.js
@@ -1,45 +1,56 @@
 /* Homepage */
 
 $(document).ready(function(){
-
+    
     // Scroll images right
-
     $('#arrow_right').click(function(){
+
         event.preventDefault();
-        let scroll = $('#preview_books').scrollLeft();
+
+        // Grab reference to the position of the scroll and increase it's value
+        var scroll = $('#popular_books_placeholder').scrollLeft();
         scroll += 290;
-        $('#preview_books').animate({scrollLeft: scroll},200);
+
+        // Apply the new value using animation
+        $('#popular_books_placeholder').animate({scrollLeft: scroll},200);
     });
     
-    // Scroll images left
 
+    // Scroll images left
     $('#arrow_left').click(function(){
+
         event.preventDefault();
-        let scroll = $('#preview_books').scrollLeft();
+
+        // Grab reference to the position of the scroll and increase it's value
+        var scroll = $('#popular_books_placeholder').scrollLeft();
         scroll -= 290;
-        $('#preview_books').animate({scrollLeft: scroll},200);
+
+        // Apply the new value using animation
+        $('#popular_books_placeholder').animate({scrollLeft: scroll},200);
     });
 
-    // Show/Hide window body
-    $('a.show_hide_button').click(function(){
+
+    // Toggle window body
+    $('a.toggle_button').click(function(){
 
         event.preventDefault();
 
+        // Grab reference to the window and the button
         var window = $(this).parents('.window');
-        var button = $(this);
+        var toggleButton = $(this);
 
         // Toggle the window body
         window
         .children('div.window_body')
         .toggle();
 
-        // Change the arrow direction
-        if(button.text() === 'keyboard_arrow_up'){
-            button.text('keyboard_arrow_down');
+        // Change the button arrow direction
+        if(toggleButton.text() === 'keyboard_arrow_up'){
+            toggleButton.text('keyboard_arrow_down');
         }else{
-            button.text('keyboard_arrow_up');
+            toggleButton.text('keyboard_arrow_up');
         };
         
-    })
+    });
 
 });
