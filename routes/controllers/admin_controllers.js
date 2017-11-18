2d228d4246fa02dc35fce858546fc6bc9f2ea55f
Darko Todoroski <daredarepp@yahoo.com>
2017-11-19 00:42:01 +0100

    Add a back button in the single item catalog windows

--- routes/controllers/admin_controllers.js
+++ routes/controllers/admin_controllers.js
@@ -3,5 +3,7 @@
 /* GET admin page. */
 
 module.exports.admin_get = function(req,res,next){
+
     res.render('admin',{title: "Admin"});
+    
 };
--- routes/controllers/catalog_controllers.js
+++ routes/controllers/catalog_controllers.js
@@ -28,6 +28,8 @@ module.exports.catalog_category_get = function(req, res, next){
 
             Book.find({}, 'title author').sort('title').populate('author', 'first_name family_name').exec(function(err, books){
 
+                if(err) throw err;
+
                 // AJAX
                 if(req.xhr){
 
@@ -39,7 +41,9 @@ module.exports.catalog_category_get = function(req, res, next){
 
                     res.render('catalog', {title: 'Catalog', category: 'Books', icon: 'library_books',
                     result: books});
+
                 };
+
             });
             break;
 
@@ -47,6 +51,7 @@ module.exports.catalog_category_get = function(req, res, next){
         case 'authors':
 
             Author.find({}, 'first_name family_name').sort('family_name').exec(function(err, authors){
+
                 if(err) throw err;
 
                 // AJAX
@@ -59,7 +64,9 @@ module.exports.catalog_category_get = function(req, res, next){
                 }else{
 
                     res.render('catalog', {title: 'Catalog', category: 'Authors', icon: 'people', result: authors});
+
                 };
+
             });
             break;
 
@@ -67,6 +74,7 @@ module.exports.catalog_category_get = function(req, res, next){
         case 'genres':
 
             Genre.find({}, 'name').sort('name').exec(function(err, genres){
+
                 if(err) throw err;
 
                 // AJAX
@@ -81,16 +89,16 @@ module.exports.catalog_category_get = function(req, res, next){
                     res.render('catalog', {title: 'Catalog', category: 'Genres', icon: 'view_agenda', result: genres});
 
                 };
+
             });
+
     };
-    
-    
+
 };
 
 /* GET single item */
 
 module.exports.catalog_item_get = function(req, res, next){
-    
     
     var uri = 'mongodb://localhost/mydb';
     var options = { useMongoClient: true };
@@ -105,19 +113,22 @@ module.exports.catalog_item_get = function(req, res, next){
         case 'books':
 
             Book.findOne({'_id': id}).populate('author', 'first_name family_name').populate('genre').exec(function(err, book){
-                if(err) throw err;
 
+                if(err) throw err;
+                
                 // AJAX
                 if(req.xhr){
-
-                    res.render('presets/catalog_presets/catalog_ajax/single_item_ajax', {book: book});
+                    
+                    res.render('presets/catalog_presets/catalog_ajax/category_ajax', {category: 'Books', 
+                    icon: 'library_books', book: book, single: true, ajax: req.xhr});
 
                 // Regular
                 }else{
 
-                    res.render('catalog', {title: 'Catalog', category: 'Books', icon: 'library_books', book: book, single: true});
+                    res.render('catalog', {title: 'Catalog', category: 'Books', icon: 'library_books', book: book, single: true, ajax: req.xhr});
 
                 };
+
             });
             break;
 
@@ -125,25 +136,29 @@ module.exports.catalog_item_get = function(req, res, next){
         case 'authors':
 
             Author.findOne({'_id': id}).exec(function(err, author){
+
                 if(err) throw err;
 
                 // Author's books
                 Book.find({'author': id}, 'title').exec(function(err, author_books){
+
                     if(err) throw err;
 
                     // AJAX
                     if(req.xhr){
     
-                        res.render('presets/catalog_presets/catalog_ajax/single_item_ajax', {author: author, author_books: author_books});
+                        res.render('presets/catalog_presets/catalog_ajax/category_ajax', {category: 'Authors', 
+                        icon: 'people', author: author, author_books: author_books, single: true, ajax: req.xhr});
     
                     // Regular
                     }else{
     
-                        res.render('catalog', {title: 'Catalog', category: 'Authors', icon: 'people', author: author, author_books: author_books, single: true});
+                        res.render('catalog', {title: 'Catalog', category: 'Authors', icon: 'people', author: author, author_books: author_books, single: true, ajax: req.xhr});
     
                     };
 
                 });
+
             });
             break;
         
@@ -151,26 +166,31 @@ module.exports.catalog_item_get = function(req, res, next){
         case 'genres':
 
             Genre.findOne({'_id': id}).exec(function(err, genre){
+
                 if(err) throw err;
                 
                 // Genre's books
                 Book.find({'genre': id}, 'title').exec(function(err, genre_books){
+
                     if(err) throw err;
 
                     // AJAX
                     if(req.xhr){
     
-                        res.render('presets/catalog_presets/catalog_ajax/single_item_ajax', {genre: genre, genre_books: genre_books});
+                        res.render('presets/catalog_presets/catalog_ajax/category_ajax', {category: 'Genres', 
+                        icon: 'view_agenda', genre: genre, genre_books: genre_books, single: true, ajax: req.xhr});
+
                     // Regular
                     }else{
     
-                        res.render('catalog', {title: 'Catalog', category: 'Genres', icon: 'view_agenda', genre: genre, genre_books: genre_books, single: true});
+                        res.render('catalog', {title: 'Catalog', category: 'Genres', icon: 'view_agenda', genre: genre, genre_books: genre_books, single: true, ajax: req.xhr});
     
                     };
 
                 });
 
             });
+            
     };
 
 };
